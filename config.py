fcb0d2884b3965f3a6e0f85cb394fff80d163aed
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2025-05-13 00:10:11 +0530

    update config

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+QDRANT_URL = os.getenv("QDRANT_URL", "http://qdrant:6333")
+REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379")
 JINA_API_KEY = os.getenv("JINA_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 POSTGRES_URL = os.getenv("POSTGRES_URL")
